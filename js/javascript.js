f1d742eb4f5366ecebd1e04b839255cb0f300134
Alexis Delgado <alexis.970991@gmail.com>
2017-04-10 07:24:44 -0700

    Evento a nav_options

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,16 @@
+
+var nav_options = document.querySelector('.nav_options');
+
 window.addEventListener('load',function(){
-  alert('Cargado..');
+
+  nav_options.addEventListener('click',function(e){
+
+    e.preventDefault();
+    var current_element = document.querySelector('.current');
+    var new_element = e.target;
+    current_element.removeAttribute('class');
+    new_element.setAttribute('class','current');
+
+  },false);
+  
 },false);
